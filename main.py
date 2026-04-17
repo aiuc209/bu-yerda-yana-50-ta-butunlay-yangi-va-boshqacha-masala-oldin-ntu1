0f697e0395850fc3d8af0a72c97e559f6e1d5594
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:49:39 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,50 @@
+1. `list(map(lambda x: x**2, [1, 2, 3, 4, 5]))`
+2. `list(map(lambda x: x.upper(), ["hello", "world"]))`
+3. `list(map(lambda x: x*2, [1, 2, 3, 4, 5]))`
+4. `list(map(lambda x: x.split(), ["hello world", "python programming"]))`
+5. `list(map(lambda x: x[::-1], ["hello", "world"]))`
+6. `list(map(lambda x: x.title(), ["hello world", "python programming"]))`
+7. `list(map(lambda x: x.strip(), ["   hello world   ", "   python programming   "]))`
+8. `list(map(lambda x: x.replace(" ", "_"), ["hello world", "python programming"]))`
+9. `list(map(lambda x: x.count("o"), ["hello world", "python programming"]))`
+10. `list(map(lambda x: len(x), ["hello", "world", "python", "programming"]))`
+11. `list(map(lambda x: x[0], ["hello", "world", "python", "programming"]))`
+12. `list(map(lambda x: x[-1], ["hello", "world", "python", "programming"]))`
+13. `list(map(lambda x: x[1:], ["hello", "world", "python", "programming"]))`
+14. `list(map(lambda x: x[:-1], ["hello", "world", "python", "programming"]))`
+15. `list(map(lambda x: x.split()[0], ["hello world", "python programming"]))`
+16. `list(map(lambda x: x.split()[-1], ["hello world", "python programming"]))`
+17. `list(map(lambda x: x.split()[1], ["hello world", "python programming"]))`
+18. `list(map(lambda x: x.split()[-2], ["hello world", "python programming"]))`
+19. `list(map(lambda x: x.replace("o", "0"), ["hello world", "python programming"]))`
+20. `list(map(lambda x: x.replace("l", "1"), ["hello world", "python programming"]))`
+21. `list(map(lambda x: x.replace("e", "3"), ["hello world", "python programming"]))`
+22. `list(map(lambda x: x.replace("a", "4"), ["hello world", "python programming"]))`
+23. `list(map(lambda x: x.replace("t", "7"), ["hello world", "python programming"]))`
+24. `list(map(lambda x: x.replace("s", "5"), ["hello world", "python programming"]))`
+25. `list(map(lambda x: x.replace("n", "9"), ["hello world", "python programming"]))`
+26. `list(map(lambda x: x.replace("r", "8"), ["hello world", "python programming"]))`
+27. `list(map(lambda x: x.replace("d", "6"), ["hello world", "python programming"]))`
+28. `list(map(lambda x: x.replace("p", "2"), ["hello world", "python programming"]))`
+29. `list(map(lambda x: x.replace("y", "1"), ["hello world", "python programming"]))`
+30. `list(map(lambda x: x.replace("h", "4"), ["hello world", "python programming"]))`
+31. `list(map(lambda x: x.replace("w", "3"), ["hello world", "python programming"]))`
+32. `list(map(lambda x: x.replace("c", "5"), ["hello world", "python programming"]))`
+33. `list(map(lambda x: x.replace("m", "6"), ["hello world", "python programming"]))`
+34. `list(map(lambda x: x.replace("b", "8"), ["hello world", "python programming"]))`
+35. `list(map(lambda x: x.replace("v", "9"), ["hello world", "python programming"]))`
+36. `list(map(lambda x: x.replace("k", "7"), ["hello world", "python programming"]))`
+37. `list(map(lambda x: x.replace("j", "2"), ["hello world", "python programming"]))`
+38. `list(map(lambda x: x.replace("g", "1"), ["hello world", "python programming"]))`
+39. `list(map(lambda x: x.replace("f", "4"), ["hello world", "python programming"]))`
+40. `list(map(lambda x: x.replace("q", "3"), ["hello world", "python programming"]))`
+41. `list(map(lambda x: x.replace("z", "5"), ["hello world", "python programming"]))`
+42. `list(map(lambda x: x.replace("x", "6"), ["hello world", "python programming"]))`
+43. `list(map(lambda x: x.replace("i", "8"), ["hello world", "python programming"]))`
+44. `list(map(lambda x: x.replace("u", "9"), ["hello world", "python programming"]))`
+45. `list(map(lambda x: x.replace("o", "7"), ["hello world", "python programming"]))`
+46. `list(map(lambda x: x.replace("l", "2"), ["hello world", "python programming"]))`
+47. `list(map(lambda x: x.replace("e", "1"), ["hello world", "python programming"]))`
+48. `list(map(lambda x: x.replace("a", "4"), ["hello world", "python programming"]))`
+49. `list(map(lambda x: x.replace("t", "3"), ["hello world", "python programming"]))`
+50. `list(map(lambda x: x.replace("s", "5"), ["hello world", "python programming"]))`
